ffef97d4b696ecb091f6ca5a757c2d5737682df1
Robert Knutsson <knutsson@gmail.com>
2014-10-02 10:04:39 +0200

    weird migration datatype

--- db/migrate/20140908085718_add_text_to_company_launches.rb
+++ db/migrate/20140908085718_add_text_to_company_launches.rb
@@ -1,5 +1,5 @@
 class AddTextToCompanyLaunches < ActiveRecord::Migration
   def change
-    add_column :company_launches, :text, :text_field
+    add_column :company_launches, :text, :text
   end
 end
