bff0df26f206498753d6692ba257d9bbf278d7bf
Robert Knutsson <knutsson@gmail.com>
2014-10-03 11:27:29 +0200

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,9 +108,9 @@ begin
     :storage => :s3,
 #    :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
     :s3_credentials => {
-      :bucket => 'newshubby-paperclip',
-      :access_key_id => ENV['S3_ACCESS_KEY'],
-      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+      bucket: 'newshubby-paperclip',
+      access_key_id: ENV['S3_ACCESS_KEY'],
+      secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
     }
   }
 end
