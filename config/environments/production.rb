fcb525cf63e449b3771dcb70bbfdd8f0f62e9bd3
Robert Knutsson <knutsson@gmail.com>
2014-12-05 10:22:24 +0100

    AWS ready

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -108,7 +108,7 @@ begin
     :storage => :s3,
 #    :s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
     :s3_credentials => {
-      bucket: 'newshubby-vpn',
+      bucket: 'newshubby-paperclip',
       access_key_id: ENV['S3_ACCESS_KEY'],
       secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
     }
