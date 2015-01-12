109860f109a1183e78e6edfdcdbbe916bbb2346d
Robert Knutsson <knutsson@gmail.com>
2015-01-12 14:41:06 +0100

    font awesome

--- config/application.rb
+++ config/application.rb
@@ -27,5 +27,11 @@ module N2
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     
+    # Add fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf ) 
+    
   end
 end
