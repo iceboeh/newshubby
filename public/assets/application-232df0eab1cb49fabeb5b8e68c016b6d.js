d1fe2918d2650d8c3ef71bcacfc01c637c5c68b3
Robert Knutsson <knutsson@gmail.com>
2015-04-01 14:15:46 +0200

    Precompiled assets

--- public/assets/application-232df0eab1cb49fabeb5b8e68c016b6d.js
+++ public/assets/application-232df0eab1cb49fabeb5b8e68c016b6d.js
@@ -32051,6 +32051,276 @@ Copyright (c) 2012-2013 Sasha Koss & Rico Sta. Cruz
 
 window.Modernizr=function(a,b,c){function G(){}function F(a,b){var c=a.charAt(0).toUpperCase()+a.substr(1),d=(a+" "+p.join(c+" ")+c).split(" ");return!!E(d,b)}function E(a,b){for(var d in a)if(k[a[d]]!==c&&(!b||b(a[d],j)))return!0}function D(a,b){return(""+a).indexOf(b)!==-1}function C(a,b){return typeof a===b}function B(a,b){return A(o.join(a+";")+(b||""))}function A(a){k.cssText=a}var d="1.7",e={},f=!0,g=b.documentElement,h=b.head||b.getElementsByTagName("head")[0],i="modernizr",j=b.createElement(i),k=j.style,l=b.createElement("input"),m=":)",n=Object.prototype.toString,o=" -webkit- -moz- -o- -ms- -khtml- ".split(" "),p="Webkit Moz O ms Khtml".split(" "),q={svg:"http://www.w3.org/2000/svg"},r={},s={},t={},u=[],v,w=function(a){var c=b.createElement("style"),d=b.createElement("div"),e;c.textContent=a+"{#modernizr{height:3px}}",h.appendChild(c),d.id="modernizr",g.appendChild(d),e=d.offsetHeight===3,c.parentNode.removeChild(c),d.parentNode.removeChild(d);return!!e},x=function(){function d(d,e){e=e||b.createElement(a[d]||"div");var f=(d="on"+d)in e;f||(e.setAttribute||(e=b.createElement("div")),e.setAttribute&&e.removeAttribute&&(e.setAttribute(d,""),f=C(e[d],"function"),C(e[d],c)||(e[d]=c),e.removeAttribute(d))),e=null;return f}var a={select:"input",change:"input",submit:"form",reset:"form",error:"img",load:"img",abort:"img"};return d}(),y=({}).hasOwnProperty,z;C(y,c)||C(y.call,c)?z=function(a,b){return b in a&&C(a.constructor.prototype[b],c)}:z=function(a,b){return y.call(a,b)},r.csstransitions=function(){return F("transitionProperty")};for(var H in r)z(r,H)&&(v=H.toLowerCase(),e[v]=r[H](),u.push((e[v]?"":"no-")+v));e.input||G(),e.crosswindowmessaging=e.postmessage,e.historymanagement=e.history,e.addTest=function(a,b){a=a.toLowerCase();if(!e[a]){b=!!b(),g.className+=" "+(b?"":"no-")+a,e[a]=b;return e}},A(""),j=l=null,e._enableHTML5=f,e._version=d,g.className=g.className.replace(/\bno-js\b/,"")+" js "+u.join(" ");return e}(this,this.document)
 ;
+/*
+ Ridiculously Responsive Social Sharing Buttons
+ Team: @dbox, @joshuatuscan
+ Site: http://www.kurtnoble.com/labs/rrssb
+ Twitter: @therealkni
+
+        ___           ___
+       /__/|         /__/\        ___
+      |  |:|         \  \:\      /  /\
+      |  |:|          \  \:\    /  /:/
+    __|  |:|      _____\__\:\  /__/::\
+   /__/\_|:|____ /__/::::::::\ \__\/\:\__
+   \  \:\/:::::/ \  \:\~~\~~\/    \  \:\/\
+    \  \::/~~~~   \  \:\  ~~~      \__\::/
+     \  \:\        \  \:\          /__/:/
+      \  \:\        \  \:\         \__\/
+       \__\/         \__\/
+*/
+
+
+
+;(function(window, jQuery, undefined) {
+	'use strict';
+
+
+	/*
+	 * Utility functions
+	 */
+	var setPercentBtns = function() {
+		// loop through each instance of buttons
+		jQuery('.rrssb-buttons').each(function(index) {
+			var self = jQuery(this);
+			var numOfButtons = jQuery('li', self).length;
+			var initBtnWidth = 100 / numOfButtons;
+
+			// set initial width of buttons
+			jQuery('li', self).css('width', initBtnWidth + '%').attr('data-initwidth',initBtnWidth);
+		});
+	};
+
+	var makeExtremityBtns = function() {
+		// loop through each instance of buttons
+		jQuery('.rrssb-buttons').each(function(index) {
+			var self = jQuery(this);
+			//get button width
+			var containerWidth = parseFloat(jQuery(self).width());
+			var buttonWidth = jQuery('li', self).not('.small').first().width();
+			var smallBtnCount = jQuery('li.small', self).length;
+
+			// enlarge buttons if they get wide enough
+			if (buttonWidth > 170 && smallBtnCount < 1) {
+				jQuery(self).addClass('large-format');
+			} else {
+				jQuery(self).removeClass('large-format');
+			}
+
+			if (containerWidth < 200) {
+				jQuery(self).removeClass('small-format').addClass('tiny-format');
+			} else {
+				jQuery(self).removeClass('tiny-format');
+			}
+		});
+	};
+
+	var backUpFromSmall = function() {
+		// loop through each instance of buttons
+		jQuery('.rrssb-buttons').each(function(index) {
+			var self = jQuery(this);
+			var totalBtnSze = 0, totalTxtSze = 0, upCandidate, nextBackUp;
+			var smallBtnCount = jQuery('li.small', self).length;
+
+			if (smallBtnCount === jQuery('li', self).length) {
+				var btnCalc = smallBtnCount * 42;
+				var containerWidth = parseFloat(jQuery(self).width());
+				upCandidate = jQuery('li.small', self).first();
+				nextBackUp = parseFloat(jQuery(upCandidate).attr('data-size')) + 55;
+
+				if ((btnCalc + nextBackUp) < containerWidth) {
+					jQuery(self).removeClass('small-format');
+					jQuery('li.small', self).first().removeClass('small');
+
+					sizeSmallBtns();
+				}
+
+			} else {
+				jQuery('li', self).not('.small').each(function(index) {
+					var txtWidth = parseFloat(jQuery(this).attr('data-size')) + 55;
+					var btnWidth = parseFloat(jQuery(this).width());
+
+					totalBtnSze = totalBtnSze + btnWidth;
+					totalTxtSze = totalTxtSze + txtWidth;
+				});
+
+				var spaceLeft = totalBtnSze - totalTxtSze;
+				upCandidate = jQuery('li.small', self).first();
+				nextBackUp = parseFloat(jQuery(upCandidate).attr('data-size')) + 55;
+
+				if (nextBackUp < spaceLeft) {
+					jQuery(upCandidate).removeClass('small');
+					sizeSmallBtns();
+				}
+			}
+		});
+	};
+
+	var checkSize = function(init) {
+		// loop through each instance of buttons
+		jQuery('.rrssb-buttons').each(function(index) {
+			//console.log('starting check for: '+(index+1));
+			var self = jQuery(this);
+			var elems = jQuery('li', self).nextAll(), count = elems.length;
+
+			// get buttons in reverse order and loop through each
+			jQuery(jQuery('li', self).get().reverse()).each(function(index, count) {
+
+				if (jQuery(this).hasClass('small') === false) {
+					var txtWidth = parseFloat(jQuery(this).attr('data-size')) + 55;
+					var btnWidth = parseFloat(jQuery(this).width());
+
+					if (txtWidth > btnWidth) {
+						//console.log($(self).attr('class')+' '+$(this).attr('class')+' txtWidth: '+txtWidth+ ' & btnWidth: '+btnWidth);
+						var btn2small = jQuery('li', self).not('.small').last();
+						jQuery(btn2small).addClass('small');
+						//console.log($(btn2small).attr('class'));
+						sizeSmallBtns();
+					}
+				}
+
+				if (!--count) backUpFromSmall();
+			});
+		});
+
+		// if first time running, put it through the magic layout
+		if (init === true) {
+			rrssbMagicLayout(sizeSmallBtns);
+		}
+	};
+
+	var sizeSmallBtns = function() {
+		// loop through each instance of buttons
+		jQuery('.rrssb-buttons').each(function(index) {
+			var self = jQuery(this);
+			var regButtonCount,
+					regPercent,
+					pixelsOff,
+					magicWidth,
+					smallBtnFraction;
+
+			// readjust buttons for small display
+			var smallBtnCount = jQuery('li.small', self).length;
+
+			// make sure there are small buttons
+			if (smallBtnCount > 0 && smallBtnCount !== jQuery('li', self).length) {
+				jQuery(self).removeClass('small-format');
+
+				//make sure small buttons are square when not all small
+				jQuery('li.small', self).css('width','42px');
+				pixelsOff = smallBtnCount * 42;
+				regButtonCount = jQuery('li', self).not('.small').length;
+				regPercent = 100 / regButtonCount;
+				smallBtnFraction = pixelsOff / regButtonCount;
+
+				if (navigator.userAgent.indexOf('Chrome') >= 0 || navigator.userAgent.indexOf('Safari') >= 0) {
+					magicWidth = '-webkit-calc('+regPercent+'% - '+smallBtnFraction+'px)';
+				} else if (navigator.userAgent.indexOf('Firefox') >= 0) {
+					magicWidth = '-moz-calc('+regPercent+'% - '+smallBtnFraction+'px)';
+				} else {
+					magicWidth = 'calc('+regPercent+'% - '+smallBtnFraction+'px)';
+				}
+				jQuery('li', self).not('.small').css('width', magicWidth);
+
+			} else if (smallBtnCount === jQuery('li', self).length) {
+				// if all buttons are small, change back to percentage
+				jQuery(self).addClass('small-format');
+				setPercentBtns();
+			} else {
+				jQuery(self).removeClass('small-format');
+				setPercentBtns();
+			}
+		}); //end loop
+
+		makeExtremityBtns();
+	};
+
+	var rrssbInit = function() {
+		jQuery('.rrssb-buttons').each(function(index) {
+			jQuery(this).addClass('rrssb-'+(index + 1));
+		});
+
+		setPercentBtns();
+
+		// grab initial text width of each button and add as data attr
+		jQuery('.rrssb-buttons li .rrssb-text').each(function(index) {
+			var txtWdth = parseFloat(jQuery(this).width());
+			jQuery(this).closest('li').attr('data-size', txtWdth);
+		});
+
+		checkSize(true);
+	};
+
+	var rrssbMagicLayout = function(callback) {
+		//remove small buttons before each conversion try
+		jQuery('.rrssb-buttons li.small').removeClass('small');
+
+		checkSize();
+
+		callback();
+	};
+
+	var popupCenter = function(url, title, w, h) {
+		// Fixes dual-screen position                         Most browsers      Firefox
+		var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+		var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+
+		var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+		var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+		var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+		var top = ((height / 3) - (h / 3)) + dualScreenTop;
+
+		var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+		// Puts focus on the newWindow
+		if (window.focus) {
+			newWindow.focus();
+		}
+	};
+
+	var waitForFinalEvent = (function () {
+		var timers = {};
+		return function (callback, ms, uniqueId) {
+			if (!uniqueId) {
+				uniqueId = "Don't call this twice without a uniqueId";
+			}
+			if (timers[uniqueId]) {
+				clearTimeout (timers[uniqueId]);
+			}
+			timers[uniqueId] = setTimeout(callback, ms);
+		};
+	})();
+
+	// init load
+	jQuery(document).ready(function(){
+		/*
+		 * Event listners
+		 */
+
+		jQuery('.rrssb-buttons a.popup').on('click', function(e){
+			var _this = jQuery(this);
+			popupCenter(_this.attr('href'), _this.find('.rrssb-text').html(), 580, 470);
+			e.preventDefault();
+		});
+
+		// resize function
+		jQuery(window).resize(function () {
+
+			rrssbMagicLayout(sizeSmallBtns);
+
+			waitForFinalEvent(function(){
+				rrssbMagicLayout(sizeSmallBtns);
+			}, 200, "finished resizing");
+		});
+
+		rrssbInit();
+	});
+
+	// Make global
+	window.rrssbInit = rrssbInit;
+
+})(window, jQuery);
 (function(jQuery){var self=null;var options={};jQuery.fn.railsAutocomplete=function(){var handler=function(){if(!this.railsAutoCompleter){this.railsAutoCompleter=new jQuery.railsAutocomplete(this)}};options[this.selector.replace("#","")]=arguments[0];if(jQuery.fn.on!==undefined){return $(document).on("focus",this.selector,handler)}else{return this.live("focus",handler)}};jQuery.railsAutocomplete=function(e){_e=e;this.init(_e)};jQuery.railsAutocomplete.fn=jQuery.railsAutocomplete.prototype={railsAutocomplete:"0.0.1"};jQuery.railsAutocomplete.fn.extend=jQuery.railsAutocomplete.extend=jQuery.extend;jQuery.railsAutocomplete.fn.extend({init:function(e){e.delimiter=jQuery(e).attr("data-delimiter")||null;function split(val){return val.split(e.delimiter)}function extractLast(term){return split(term).pop().replace(/^\s+/,"")}jQuery(e).autocomplete($.extend({source:function(request,response){jQuery.getJSON(jQuery(e).attr("data-autocomplete"),{term:extractLast(request.term)},function(){if(arguments[0].length==0){arguments[0]=[];arguments[0][0]={id:"",label:"no existing match"}}jQuery(arguments[0]).each(function(i,el){var obj={};obj[el.id]=el;jQuery(e).data(obj)});response.apply(null,arguments)})},change:function(event,ui){if(jQuery(jQuery(this).attr("data-id-element")).val()==""){return}jQuery(jQuery(this).attr("data-id-element")).val(ui.item?ui.item.id:"");var update_elements=jQuery.parseJSON(jQuery(this).attr("data-update-elements"));var data=ui.item?jQuery(this).data(ui.item.id.toString()):{};if(update_elements&&jQuery(update_elements.id).val()==""){return}for(var key in update_elements){jQuery(update_elements[key]).val(ui.item?data[key]:"")}},search:function(){var term=extractLast(this.value);if(term.length<2){return false}},focus:function(){return false},select:function(event,ui){var terms=split(this.value);terms.pop();terms.push(ui.item.value);if(e.delimiter!=null){terms.push("");this.value=terms.join(e.delimiter)}else{this.value=terms.join("");if(jQuery(this).attr("data-id-element")){jQuery(jQuery(this).attr("data-id-element")).val(ui.item.id)}if(jQuery(this).attr("data-update-elements")){var data=jQuery(this).data(ui.item.id.toString());var update_elements=jQuery.parseJSON(jQuery(this).attr("data-update-elements"));for(var key in update_elements){jQuery(update_elements[key]).val(data[key])}}}var remember_string=this.value;jQuery(this).bind("keyup.clearId",function(){if(jQuery(this).val().trim()!=remember_string.trim()){jQuery(jQuery(this).attr("data-id-element")).val("");jQuery(this).unbind("keyup.clearId")}});jQuery(e).trigger("railsAutocomplete.select",ui);return false}},options[e.id]))}});jQuery(document).ready(function(){jQuery("input[data-autocomplete]").railsAutocomplete()})})(jQuery);
 (function() {
   var CSRFToken, Click, ComponentUrl, EVENTS, Link, ProgressBar, browserIsntBuggy, browserSupportsCustomEvents, browserSupportsPushState, browserSupportsTurbolinks, bypassOnLoadPopstate, cacheCurrentPage, cacheSize, changePage, clone, constrainPageCacheTo, createDocument, crossOriginRedirect, currentState, enableProgressBar, enableTransitionCache, executeScriptTags, extractTitleAndBody, fetch, fetchHistory, fetchReplacement, historyStateIsDefined, initializeTurbolinks, installDocumentReadyPageEventTriggers, installHistoryChangeHandler, installJqueryAjaxSuccessPageUpdateTrigger, loadedAssets, manuallyTriggerHashChangeForFirefox, pageCache, pageChangePrevented, pagesCached, popCookie, processResponse, progressBar, recallScrollPosition, referer, reflectNewUrl, reflectRedirectedUrl, rememberCurrentState, rememberCurrentUrl, rememberReferer, removeNoscriptTags, requestMethodIsSafe, resetScrollPosition, setAutofocusElement, transitionCacheEnabled, transitionCacheFor, triggerEvent, visit, xhr, _ref,
@@ -33110,6 +33380,21 @@ $(document).ready(function() {
     return event.preventDefault();
   });
 
+  $(document).ready(function() {
+    $('#prStateBtn').click(function() {
+      $('.prState').toggleClass('text-info text-primary');
+      $('#prStateBtn').toggleClass('btn-info btn-primary');
+      $('.prLink').toggleClass('hidden');
+      $('.prStateText').text(function(i, v) {
+        if (v === 'unlisted') {
+          return 'public';
+        } else {
+          return 'unlisted';
+        }
+      });
+    });
+  });
+
 }).call(this);
 (function() {
   var subscription;
@@ -33165,6 +33450,7 @@ $(document).ready(function() {
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 
 
 
